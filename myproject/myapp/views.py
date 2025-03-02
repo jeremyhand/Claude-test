6ffd72dc4722d6f22937532b2206f78c43a006bf
Jeremy Hand <jeremyhand3.14@gmail.com>
2025-03-01 18:44:35 -0600

    docker

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,36 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, 'home.html')
+
+def products(request):
+    products_list = Product.objects.all()
+    cart = request.session.get('cart', {})
+    cart_count = sum(cart.values())
+    
+    context = {
+        'products': products_list,
+        'cart_count': cart_count
+    }
+    return render(request, 'products.html', context)
+
+def add_to_cart(request, product_id):
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+        product_id_str = str(product_id)
+        
+        if product_id_str in cart:
+            cart[product_id_str] += 1
+        else:
+            cart[product_id_str] = 1
+            
+        request.session['cart'] = cart
+        
+        return JsonResponse({
+            'success': True,
+            'cart_count': sum(cart.values())
+        })
+    
+    return JsonResponse({'success': False})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from myapp.views import home
+from myapp.views import home, products, add_to_cart
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # New URL mapping
+    path('', home, name='home'),  # This maps the root URL to your home view
+    path('products/', products, name='products'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 ]
